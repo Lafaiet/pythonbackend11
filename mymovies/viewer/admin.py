8e065b3764eb9d42a117e348169127861185d6aa
Lafaiet Castro e Silva <lafaietsilva@gmail.com>
2022-10-01 14:36:10 +0300

    Adding models relations

--- mymovies/viewer/admin.py
+++ mymovies/viewer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from viewer.models import Movie, Director, Actor
 
-# Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Director)
+admin.site.register(Actor)
--- mymovies/viewer/models.py
+++ mymovies/viewer/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
-# Movie
-# Director
-# Star
-# Genre ?
 # Music
-# User review
-# User
+# User review ?
+# User ?
+
+
+class Director(models.Model):
+
+    name = models.CharField(max_length=50)
+    date_birth = models.DateField()
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+
+    name = models.CharField(max_length=50)
+    date_birth = models.DateField()
+    bio = models.TextField(null=True, blank=True)
+    place_birth = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
@@ -15,7 +32,13 @@ class Movie(models.Model):
     release_year = models.PositiveIntegerField()
     duration = models.DurationField()
     genre = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
+    rating = models.FloatField(null=True, blank=True)
+    date_added = models.DateTimeField(auto_now=True, null=True, blank=True)
+    director = models.ForeignKey(Director, on_delete=models.DO_NOTHING, null=True, blank=True)
+    actors = models.ManyToManyField(Actor, blank=True)
 
-    # director ?
-    # actors ?
+    def __str__(self):
+        return self.title
+
+    # stars ?
