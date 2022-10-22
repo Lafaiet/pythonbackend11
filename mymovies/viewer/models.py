164fc7a9ccf6771199341858bbc3b943d08891c9
Lafaiet Castro e Silva <lafaietsilva@gmail.com>
2022-10-22 12:54:44 +0300

    Adding choices

--- mymovies/viewer/models.py
+++ mymovies/viewer/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from viewer.choices import *
 
 # Music
 # User review ?
@@ -26,19 +27,24 @@ class Actor(models.Model):
     def __str__(self):
         return self.name
 
+GENRES_CHOICES = [
+    ['ACTION', 'ACTION'],
+    ['COMEDY', 'COMEDY'],
+]
 
 class Movie(models.Model):
 
     title = models.CharField(max_length=100)
     release_year = models.PositiveIntegerField()
     duration = models.DurationField()
-    genre = models.CharField(max_length=30)
+    genre = models.CharField(max_length=30, choices=GENRES_CHOICES)
     description = models.TextField(null=True, blank=True)
     rating = models.FloatField(null=True, blank=True)
     date_added = models.DateTimeField(auto_now=True, null=True, blank=True)
     director = models.ForeignKey(Director, on_delete=models.DO_NOTHING, null=True, blank=True)
     actors = models.ManyToManyField(Actor, blank=True)
     # cover = models.ImageField(null=True, blank=True, upload_to='images/')
+    language = models.CharField(max_length=50, null=True, blank=True, choices=LANGUAGES)
 
     def __str__(self):
         return self.title
